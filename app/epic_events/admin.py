45253598d5ae7b29986f40e6e5fd75b4a6e14420
Maxime Soydas <max.soydas@gmail.com>
2022-09-22 22:57:09 +0200

    user groups + admin auth + models setup

--- app/epic_events/admin.py
+++ app/epic_events/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from epic_events.models import Client, Contract, ContractStatus, Event
 
+
+
+# class ClientInline(admin.TabularInline):
+#      model = Client
 # Register your models here.
+# @admin.register(Client)
+# class GenericAdmin(admin.ModelAdmin):
+#     pass
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display= ('first_name','last_name')
+    # inlines = (ClientInline)
+    
+    
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display= ('sales_contact','client','status')
+
+@admin.register(ContractStatus)
+class ContractStatusAdmin(admin.ModelAdmin):
+    list_display= ('CHOICES',)
+    
+    
+@admin.register(Event)
+class Event(admin.ModelAdmin):
+    list_display= ('client','support_contact','event_status')
+
+
+
+
--- app/epic_events/models.py
+++ app/epic_events/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
 class Client(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -8,6 +9,40 @@ class Client(models.Model):
     phone = models.CharField(max_length=100)
     mobile = models.CharField(max_length=100)
     company_name = models.CharField(max_length=100)
-    date_create = models.DateTimeField()
-    date_updated = models.DateTimeField()
-    sales_contact = models.CharField(max_length=100)
+    date_create = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    sales_contact = models.ForeignKey(User,on_delete=models.CASCADE,limit_choices_to={'groups__name': "sales"})
+    
+class Contract(models.Model):
+    sales_contact = models.ForeignKey(User, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date_create = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField()
+    amount = models.FloatField()
+    payment_due = models.DateTimeField()
+    
+    
+class ContractStatus(models.Model):
+    OK = 'ok'
+    PENDING = 'pending'
+    FAILED = 'failed'
+
+    CHOICES = (
+        (OK, 'Ok'),
+        (PENDING, 'Pending'),
+        (FAILED, 'Failed'),
+    )
+
+    status = models.CharField(max_length=255, choices=CHOICES, default=PENDING)
+    
+class Event(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date_create = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    support_contact = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'groups__name': "support"})
+    event_status = models.ForeignKey(ContractStatus, on_delete=models.CASCADE)
+    attendees = models.IntegerField()
+    event_date = models.DateTimeField()
+    notes = models.TextField(blank=True)
+    
